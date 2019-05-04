345cfc644a793807c884c03fd38368a0c28a0006
Ling Jiang <kyooryoo@hotmail.com>
2019-05-04 03:04:57 +0000

    Finish user signup and use SSL and Puma in production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+    #debugger
   end
+  
+  def show
+    @user = User.find(params[:id])
+    #debugger
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Your new account is created!"
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
