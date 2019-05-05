2da68563d70a1b1056d745673945fe5c8a784eaa
Ling Jiang <kyooryoo@hotmail.com>
2019-05-05 07:26:47 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Your new account is created!"
       redirect_to user_path(@user)
     else
