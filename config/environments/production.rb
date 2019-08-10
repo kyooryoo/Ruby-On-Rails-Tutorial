c673ab95539e212da9639985b07c075267aa76f1
Ling Jiang <kyooryoo@hotmail.com>
2019-08-10 19:06:17 +0800

    env prod bug fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   # host = 'https://learn-rails-sample-app-2019.herokuapp.com'
-  config.action_mailer.default_url_options = { host: ror2019.herokuapp.com }
+  config.action_mailer.default_url_options = { host: 'ror2019.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
